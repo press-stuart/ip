ac08791d26d8468447b7a1614a97c37a3781ead3
Stuart Lim <61983502+press-stuart@users.noreply.github.com>
2026-01-23 19:48:20 +0800

    Add input parsing method to chatbot

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Bobby {
@@ -31,6 +32,54 @@ public class Bobby {
 
         System.out.println(frameIndent + horizontalLine);
         System.out.println();
+    }
+
+    /**
+     * Parses a line of user input to extract the command, value and parameters.
+     * 
+     * The command is the first word, using whitespace characters as delimiters. The value is the
+     * text between the command and the first parameter, or the end of the string if no parameters
+     * exist.
+     * 
+     * Forward slashes `/` define the beginnings of parameters. After removing the forward slash,
+     * the first word is the parameter name. All remaining text forms the parameter value.
+     * 
+     * @param line String containing a line of user input.
+     * @return A HashMap
+     */
+    private static HashMap<String, String> parse(String line) {
+        HashMap<String, String> components = new HashMap<>();
+
+        if (line == null || line.isBlank()) {
+            // TODO: exception handling
+            return components;
+        }
+
+        // Split by one or more whitespace characters followed by '/'
+        String[] sections = line.split("\\s+/");
+
+        String commandValueSection = sections[0];
+        String[] commandValueTokens = commandValueSection.split("\\s+", 2);
+
+        // TODO: exception handling
+        String command = commandValueTokens[0];
+        components.put("command", command);
+
+        String value = commandValueTokens.length < 2 ? "" : commandValueTokens[1];
+        components.put("value", value);
+
+        // Process additional parameters
+        for (int i = 1; i < sections.length; i++) {
+            String parameterSection = sections[i];
+            String[] parts = parameterSection.split("\\s+", 2);
+
+            // TODO: exception handling
+            String parameterName = parts[0];
+            String parameterValue = parts.length < 2 ? "" : parts[1];
+            components.put(parameterName, parameterValue);
+        }
+
+        return components;
     }
 
     public static void main(String[] args) {
