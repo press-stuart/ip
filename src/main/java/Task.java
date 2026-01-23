f6eff847311ee2bdf6834a9bfa49d42e07215092
Stuart Lim <61983502+press-stuart@users.noreply.github.com>
2026-01-23 14:13:35 +0800

    Add doneness member to Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
 public class Task {
     protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
     }
 
     @Override
     public String toString() {
-        return this.description;
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
     }
 }
