aea8f9f1d15bca64260be5a8763526b4e6349f67
Stuart Lim <61983502+press-stuart@users.noreply.github.com>
2026-02-01 14:52:31 +0800

    Extract code out of Bobby.main

--- src/main/java/Bobby.java
+++ src/main/java/Bobby.java
@@ -11,126 +11,28 @@ public class Bobby {
     /** Length of each horizontal line frame. */
     private static final int FRAME_LENGTH = 67;
 
+    /** Scanner to read user input. */
+    private Scanner sc;
+
     /** List of user tasks. */
-    private static TaskList taskList;
-
-    /** Indicates whether the user finished interaction. */
-    private static boolean isFinished;
-
-    /**
-     * Prints the given message with indentation and horizontal lines above and below the message.
-     */
-    private static void printMessage(String message) {
-        String frameIndent = " ".repeat(FRAME_INDENTATION);
-        String textIndent = " ".repeat(TEXT_INDENTATION);
-        String horizontalLine = "_".repeat(FRAME_LENGTH);
-
-        String[] messageLines = message.split("\n");
-
-        System.out.println(frameIndent + horizontalLine);
-
-        for (String line : messageLines) {
-            System.out.println(textIndent + line);
-        }
-
-        System.out.println(frameIndent + horizontalLine);
-        System.out.println();
-    }
-
-    private static void runByeCommand() {
-        isFinished = true;
-    }
-
-    private static void runListCommand() {
-        printMessage("Tasks in your list:\n" + taskList.toString());
-    }
-
-    private static void runMarkCommand(HashMap<String, String> inputParts) {
-        int taskIndex = Integer.valueOf(inputParts.get("value"));
-        Task task = taskList.getTask(taskIndex);
-        task.markDone();
-        printMessage("Marked this task as done:\n  " + task);
-    }
-
-    private static void runUnmarkCommand(HashMap<String, String> inputParts) {
-        int taskIndex = Integer.valueOf(inputParts.get("value"));
-        Task task = taskList.getTask(taskIndex);
-        task.unmarkDone();
-        printMessage("Marked this task as not done:\n  " + task);
-    }
-
-    private static void runDeleteCommand(HashMap<String, String> inputParts) {
-        int taskIndex = Integer.valueOf(inputParts.get("value"));
-        Task deletedTask = taskList.deleteTask(taskIndex);
-        printMessage(String.format(
-                "Deleted this task:\n  %s\nNow you have %d tasks in the list.",
-                deletedTask.toString(), taskList.getSize()));
-    }
-
-    private static void runTodoCommand(HashMap<String, String> inputParts)
-            throws DukeException {
-        String description = inputParts.get("value");
-        if (description == null || description.isEmpty()) {
-            throw new DukeException("The description of a todo cannot be empty!");
-        }
-
-        Todo todo = new Todo(description);
-        taskList.addTask(todo);
-        printMessage(String.format(
-                "Added this task:\n  %s\nNow you have %d tasks in the list.",
-                todo.toString(), taskList.getSize()));
-    }
-
-    private static void runDeadlineCommand(HashMap<String, String> inputParts)
-            throws DukeException {
-        String description = inputParts.get("value");
-        if (description == null || description.isEmpty()) {
-            throw new DukeException("The description of a deadline cannot be empty!");
-        }
-
-        String by = inputParts.get("by");
-        if (by == null || by.isEmpty()) {
-            throw new DukeException("I couldn't find the deadline!\n"
-                    + "(Hint: Use the /by parameter)");
-        }
-
-        Deadline deadline = new Deadline(description, by);
-        taskList.addTask(deadline);
-        printMessage(String.format(
-                "Added this task:\n  %s\nNow you have %d tasks in the list.",
-                deadline.toString(), taskList.getSize()));
-    }
-
-    private static void runEventCommand(HashMap<String, String> inputParts)
-            throws DukeException {
-        String description = inputParts.get("value");
-        if (description == null || description.isEmpty()) {
-            throw new DukeException("The description of an event cannot be empty!");
-        }
-
-        String from = inputParts.get("from");
-        if (from == null || from.isEmpty()) {
-            throw new DukeException("I couldn't find the start time!\n"
-                    + "(Hint: Use the /from parameter)");
-        }
-
-        String to = inputParts.get("to");
-        if (to == null || to.isEmpty()) {
-            throw new DukeException("I couldn't find the end time!\n"
-                    + "(Hint: Use the /to parameter)");
-        }
-
-        Event event = new Event(description, from, to);
-        taskList.addTask(event);
-        printMessage(String.format(
-                "Added this task:\n  %s\nNow you have %d tasks in the list.",
-                event.toString(), taskList.getSize()));
-    }
+    private TaskList taskList;
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Bobby bobby = new Bobby();
+        bobby.runLoopUntilExit();
+        bobby.cleanUpAfterExit();
+    }
+
+    public Bobby() {
+        sc = new Scanner(System.in);
         taskList = new TaskList();
-        isFinished = false;
+    }
+
+    /**
+     * Reads and executes user commands until the exit command is found.
+     */
+    public void runLoopUntilExit() {
+        boolean isFinished = false;
 
         printMessage("Hello! I'm Bobby.\nWhat can I do for you?");
 
@@ -142,7 +44,7 @@ public class Bobby {
                 String command = inputParts.get("command");
 
                 if (command.equalsIgnoreCase("bye")) {
-                    runByeCommand();
+                    isFinished = true;
                 } else if (command.equalsIgnoreCase("list")) {
                     runListCommand();
                 } else if (command.equalsIgnoreCase("mark")) {
@@ -167,7 +69,112 @@ public class Bobby {
         }
 
         printMessage("Bye! Hope to see you again soon!");
+    }
 
+    private void cleanUpAfterExit() {
         sc.close();
+    }
+
+    /**
+     * Prints the given message with indentation and horizontal lines above and below the message.
+     */
+    private static void printMessage(String message) {
+        String frameIndent = " ".repeat(FRAME_INDENTATION);
+        String textIndent = " ".repeat(TEXT_INDENTATION);
+        String horizontalLine = "_".repeat(FRAME_LENGTH);
+
+        String[] messageLines = message.split("\n");
+
+        System.out.println(frameIndent + horizontalLine);
+
+        for (String line : messageLines) {
+            System.out.println(textIndent + line);
+        }
+
+        System.out.println(frameIndent + horizontalLine);
+        System.out.println();
+    }
+
+    private void runListCommand() {
+        printMessage("Tasks in your list:\n" + taskList.toString());
+    }
+
+    private void runMarkCommand(HashMap<String, String> inputParts) {
+        int taskIndex = Integer.valueOf(inputParts.get("value"));
+        Task task = taskList.getTask(taskIndex);
+        task.markDone();
+        printMessage("Marked this task as done:\n  " + task);
+    }
+
+    private void runUnmarkCommand(HashMap<String, String> inputParts) {
+        int taskIndex = Integer.valueOf(inputParts.get("value"));
+        Task task = taskList.getTask(taskIndex);
+        task.unmarkDone();
+        printMessage("Marked this task as not done:\n  " + task);
+    }
+
+    private void runDeleteCommand(HashMap<String, String> inputParts) {
+        int taskIndex = Integer.valueOf(inputParts.get("value"));
+        Task deletedTask = taskList.deleteTask(taskIndex);
+        printMessage(String.format(
+                "Deleted this task:\n  %s\nNow you have %d tasks in the list.",
+                deletedTask.toString(), taskList.getSize()));
+    }
+
+    private void runTodoCommand(HashMap<String, String> inputParts) throws DukeException {
+        String description = inputParts.get("value");
+        if (description == null || description.isEmpty()) {
+            throw new DukeException("The description of a todo cannot be empty!");
+        }
+
+        Todo todo = new Todo(description);
+        taskList.addTask(todo);
+        printMessage(String.format(
+                "Added this task:\n  %s\nNow you have %d tasks in the list.",
+                todo.toString(), taskList.getSize()));
+    }
+
+    private void runDeadlineCommand(HashMap<String, String> inputParts) throws DukeException {
+        String description = inputParts.get("value");
+        if (description == null || description.isEmpty()) {
+            throw new DukeException("The description of a deadline cannot be empty!");
+        }
+
+        String by = inputParts.get("by");
+        if (by == null || by.isEmpty()) {
+            throw new DukeException("I couldn't find the deadline!\n"
+                    + "(Hint: Use the /by parameter)");
+        }
+
+        Deadline deadline = new Deadline(description, by);
+        taskList.addTask(deadline);
+        printMessage(String.format(
+                "Added this task:\n  %s\nNow you have %d tasks in the list.",
+                deadline.toString(), taskList.getSize()));
+    }
+
+    private void runEventCommand(HashMap<String, String> inputParts) throws DukeException {
+        String description = inputParts.get("value");
+        if (description == null || description.isEmpty()) {
+            throw new DukeException("The description of an event cannot be empty!");
+        }
+
+        String from = inputParts.get("from");
+        if (from == null || from.isEmpty()) {
+            throw new DukeException("I couldn't find the start time!\n"
+                    + "(Hint: Use the /from parameter)");
+        }
+
+        String to = inputParts.get("to");
+        if (to == null || to.isEmpty()) {
+            throw new DukeException("I couldn't find the end time!\n"
+                    + "(Hint: Use the /to parameter)");
+        }
+
+        Event event = new Event(description, from, to);
+        taskList.addTask(event);
+        printMessage(String.format(
+                "Added this task:\n  %s\nNow you have %d tasks in the list.",
+                event.toString(), taskList.getSize()));
     }
 }
