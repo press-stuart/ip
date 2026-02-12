ec9ee5a9eec67d70254323eb3dbe55f72a1abc9c
Stuart Lim <61983502+press-stuart@users.noreply.github.com>
2026-02-13 03:53:02 +0800

    Add GUI

---
+++ src/main/java/bobby/Main.java
@@ -0,0 +1,40 @@
+package bobby;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import bobby.ui.MainWindow;
+
+/**
+ * A GUI for Bobby using FXML.
+ */
+public class Main extends Application {
+
+    private Bobby bobby = new Bobby();
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            MainWindow controller = fxmlLoader.getController();
+            controller.setBobby(bobby); // inject the Bobby instance
+            stage.show();
+            controller.showIntroMessage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void stop() {
+        bobby.cleanUpAfterExit();
+    }
+}
